8fbc914c57218a4ad0a0f2b792bc06a97ac3e6b4
redjax <jackenyon@gmail.com>
2024-04-28 01:16:24 -0400

    Fix current comic scheduled task

--- src/auto_xkcd/celeryapp/celery_tasks/scheduled/tasks.py
+++ src/auto_xkcd/celeryapp/celery_tasks/scheduled/tasks.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 ## Check for new XKCD comic every hour
 TASK_SCHEDULE_current_comic_check = {
     "hourly_current_comic_check": {
-        "task": "celeryapp.celery_tasks.comic.task_current_comic",
-        "schedule": crontab(hour="*", minute=0),
+        "task": "request_current_comic",
+        "schedule": crontab(hour="*", minute="*"),
     }
 }
