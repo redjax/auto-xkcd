c77666343a135315bce8c0c1882fef475e11e48e
redjax <no@none.com>
2025-04-25 23:33:48 -0400

    Add dynaconf settings objects.
    This emulates the individually loaded objects in individual Python modules I was using before. Much cleaner.

---
+++ libs/settings-lib/src/settings/__settings.py
@@ -0,0 +1,17 @@
+from .base import get_namespace
+
+__all__ = [
+    "LOGGING_SETTINGS",
+    "APP_SETTINGS",
+    "DATABASE_SETTINGS",
+    "CELERY_SETTINGS",
+    "FASTAPI_SETTINGS",
+    "UVICORN_SETTINGS",
+]
+
+LOGGING_SETTINGS = get_namespace("logging")
+APP_SETTINGS = get_namespace("app")
+DATABASE_SETTINGS = get_namespace("database")
+CELERY_SETTINGS = get_namespace("celery")
+FASTAPI_SETTINGS = get_namespace("fastapi")
+UVICORN_SETTINGS = get_namespace("uvicorn")
