33142159931d2289110dc3c7a5c68cc8902b1773
redjax <no@none.com>
2025-04-26 22:20:22 -0400

    Add alembic, setup CLI modules.

--- applications/project-cli/src/project_cli/main.py
+++ applications/project-cli/src/project_cli/main.py
@@ -5,6 +5,8 @@ import typing as t
 
 from .celery import celery_app
 from .db import db_app
+from ._alembic import alembic_app
+from .setup import setup_app
 
 # from .weather import weather_app
 from cyclopts import App, Group, Parameter
@@ -14,29 +16,42 @@ app = App(name="weathercli", help="CLI for WeatherData app.")
 
 app.meta.group_parameters = Group("Session Parameters", sort_key=0)
 
-MOUNT_SUB_CLIS: list = [celery_app, db_app]
+MOUNT_SUB_CLIS: list = [celery_app, db_app, alembic_app, setup_app]
 
 ## Mount apps
 for sub_cli in MOUNT_SUB_CLIS:
     app.command(sub_cli)
 
+
 @app.meta.default
-def cli_launcher(*tokens: t.Annotated[str, Parameter(show=False, allow_leading_hyphen=True)], debug: bool = False):
+def cli_launcher(
+    *tokens: t.Annotated[str, Parameter(show=False, allow_leading_hyphen=True)],
+    debug: bool = False,
+):
     """CLI entrypoint.
-    
+
     Params:
         debug (bool): If `True`, enables debug logging.
     """
     # log.remove(0)
-    
+
     if debug:
-        log.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} | [{level}] | {name}.{function}:{line} |> {message}", level="DEBUG")
-        
+        log.add(
+            sys.stderr,
+            format="{time:YYYY-MM-DD HH:mm:ss} | [{level}] | {name}.{function}:{line} |> {message}",
+            level="DEBUG",
+        )
+
         log.debug("CLI debugging enabled.")
     else:
-        log.add(sys.stderr, format="{time:YYYY-MM-DD HH:mm:ss} [{level}] : {message}", level="INFO")
-                
+        log.add(
+            sys.stderr,
+            format="{time:YYYY-MM-DD HH:mm:ss} [{level}] : {message}",
+            level="INFO",
+        )
+
     app(tokens)
+
 
 if __name__ == "__main__":
     app()
